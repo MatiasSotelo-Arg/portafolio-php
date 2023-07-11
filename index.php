0bb4bc66d9b2d2bfa8165a404b775338b54484b2
Matias Sotelo <matysotelo07@gmail.com>
2023-07-11 17:46:25 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portafolio Matias Sotelo</title>
+
+    <link rel="stylesheet" href="./css/resets.css">
+</head>
+<body>
+    <?php
+
+        include './pages/header/header.php';
+
+        include './pages/main/main.php';
+
+        include './pages/footer/footer.php';
+    ?> 
+</body>
+</html>
---
+++ pages/header/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portafolio php</title>
+
+    <link rel="stylesheet" href="./pages/header/header.css">
+</head>
+<body>
+    <header class="contenedor-header"> 
+
+        <nav class="nav">
+            <h1 class="titulo">
+                <a href="index.php">&lt;MS/&gt; DEV</a>
+            </h1>
+
+            <div>
+                <ul>
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Proyectos</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </ul>
+                
+            </div>
+            
+        </nav>
+    </header>
+</body>
+</html>
